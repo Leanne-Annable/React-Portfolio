056266acea84ffb6663b4e80448d77dea790ff0d
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-03-03 15:21:57 +0000

    set up basic files and installed react, react router and bootstrap

---
+++ reactportfolio/src/components/NavTabs.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        {/* NavLink is the same as using an <a> tag e.g <a href="/about">About</a>*/}
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="projects"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Projects
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ reactportfolio/src/components/pages/projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>
+        This is a selection of my previous Challenges that I created during my bootcamp
+      </p>
+    </div>
+  );
+}
+
+export default Projects;
